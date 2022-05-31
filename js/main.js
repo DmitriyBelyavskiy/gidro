c91fe0174249ab5b91508b5f2d7544e38303fb37
DmitriyBelyavskiy <79460108+DmitriyBelyavskiy@users.noreply.github.com>
2022-05-31 20:49:15 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,119 @@
+$(function () {
+    $('.banner-section__slider').slick({
+
+        dots: true,
+        prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrowleft.svg" alt=""></button>',
+
+        nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrowright.svg" alt=""></button>',
+        responsive: [
+            {
+              breakpoint: 969,
+              settings: {
+                arrows: false,
+              }
+            }
+
+        ]
+    });
+    $('.tab').on('click', function (e) {
+        e.preventDefault();
+
+        $($(this).siblings()).removeClass('tab--active');
+        $($(this).closest('.tabs__wrapper').siblings().find('div')).removeClass('tabs-content--active');
+
+
+        $(this).addClass('tab--active');
+        $($(this).attr('href')).addClass('tabs-content--active')
+
+
+        $('.product-slider').slick('setPosition');
+
+    });
+
+    $('.product-item__lovlely').on('click', function () {
+        $(this).toggleClass('product-item__lovlely--active')
+    });
+
+    $('.product-slider').slick({
+        slidesToShow: 4,
+        slidesToscroll: 1,
+        prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/products-arrow-leftsvg.svg" alt=""></button>',
+        nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/products-arrow-right.svg" alt=""></button>',
+        responsive: [
+            {
+              breakpoint: 1301,
+              settings: {
+                arrows: false,
+                dots: true,
+              }
+            },
+            {
+                breakpoint: 1201,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 3,
+                    dots: true,
+                }
+              },
+              {
+                breakpoint: 870,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 2,
+                    dots: true,
+                }
+              },
+              {
+                breakpoint: 590,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                    dots: true,
+                }
+              }
+
+        ]
+        
+    });
+
+    $('.filter-style').styler();
+
+    $('.filter__item-drop, .filter__extra').on('click', function(){
+        $(this).toggleClass('filter__item-drop--active');
+        $(this).next().slideToggle('200')
+    });
+    $('.js-range-slider').ionRangeSlider({
+        type: "double",
+        min:100000,
+        max:500000,
+    });
+
+    $('.catalog__filter-btngrid').on('click', function(){
+        $(this).toggleClass('catalog__filter-button--active');
+        $('.catalog__filter-btnline').removeClass('catalog__filter-button--active');
+        $('.product-item__wrapper').removeClass('product-item__wrapper--list');
+    });
+    $('.catalog__filter-btnline').on('click', function(){
+        $(this).toggleClass('catalog__filter-button--active');
+        $('.catalog__filter-btngrid').removeClass('catalog__filter-button--active');
+        $('.product-item__wrapper').addClass('product-item__wrapper--list');
+    });
+    $(".rate-yo").rateYo({
+        ratedFill: "#1C62CD",
+        spacing: "7px",
+        normalFill: "#C4C4C4"
+      });
+      $('.menu__btn').on('click',function(){
+        $('.menu-mobile__list').toggleClass('menu-mobile__list--active')
+      });
+
+      $('.footer__topdrop').on('click', function(){
+        $(this).next().slideToggle();
+        $(this).toggleClass('footer__topdrop--active');
+      });
+      $('.aside__btn').on('click', function(){
+        $(this).next().slideToggle();
+      });
+
+
+});
